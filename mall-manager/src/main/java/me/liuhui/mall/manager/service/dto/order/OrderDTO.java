65f62f77e3686643b6837c948477efbd773fca41
linkaiqiang <linkaiqiang@haohandata.com.cn>
2023-04-11 18:32:52 +0800

    提交代码

---
+++ mall-manager/src/main/java/me/liuhui/mall/manager/service/dto/order/OrderDTO.java
@@ -0,0 +1,13 @@
+package me.liuhui.mall.manager.service.dto.order;
+
+import lombok.Data;
+
+/**
+ * @author lin
+ */
+@Data
+public class OrderDTO {
+
+    private Long productId;
+    private Integer saleNum;
+}
